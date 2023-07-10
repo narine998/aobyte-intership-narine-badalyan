2eca9e7ae3dea5ab6cd287974e4dab2663648d40
Narine <68425891+narine998@users.noreply.github.com>
2023-07-10 10:48:25 +0400

    Approve pull request

--- homework-3/scripts/triangle.js
+++ homework-3/scripts/triangle.js
@@ -1,22 +1,23 @@
-function renderTriangle(size) {
-  const space = " ";
-  const starSpace = " *";
+const DELIMITER = " ";
+const SYMBOL = "*";
+
+function renderTriangle(size, delimiter, symbol) {
+  const sign = delimiter + symbol;
   let triangleString = "";
   for (let i = 0; i < size; i++) {
     triangleString +=
-      space.repeat(size - i - 1) + starSpace.repeat(i + 1) + "\n";
+      delimiter.repeat(size - i - 1) + sign.repeat(i + 1) + "\n";
   }
   return triangleString;
 }
 
-function rotateTriangle(size) {
-  const space = " ";
-  const starSpace = " *";
+function rotateTriangle(size, delimiter, symbol) {
+  const sign = delimiter + symbol;
   let reversedTriangleString = "";
 
   for (let i = size; i > 0; i--) {
     reversedTriangleString +=
-      space.repeat(size - i) + starSpace.repeat(i) + "\n";
+      delimiter.repeat(size - i) + sign.repeat(i) + "\n";
   }
 
   return reversedTriangleString;
@@ -25,18 +26,35 @@ function rotateTriangle(size) {
 const inputNode = document.getElementById("triangle-length-input");
 const rotateButtonNode = document.getElementById("rotate-btn");
 
+const validationNode = document.getElementById("validation");
 const preNode = document.getElementById("equal-triangle");
 const reversedPreNode = document.getElementById("rotated-triangle");
 
 inputNode.addEventListener("keypress", (e) => {
+  validationNode.innerText = "";
   if (e.key === "Enter") {
     e.preventDefault();
     reversedPreNode.innerText = "";
-    preNode.innerText = renderTriangle(inputNode.value);
-    rotateButtonNode.style.visibility = "visible";
+    const size = +e.target.value;
+    if (size <= 0 || size !== Math.floor(size)) {
+      rotateButtonNode.style.visibility = "hidden";
+      preNode.innerText = "";
+      validationNode.innerText = "Length can only be positive integer";
+    } else {
+      try {
+        preNode.innerText = renderTriangle(size, DELIMITER, SYMBOL);
+        rotateButtonNode.style.visibility = "visible";
+      } catch (err) {
+        validationNode.innerText = err.message;
+      }
+    }
   }
 });
 
 rotateButtonNode.addEventListener("click", () => {
-  reversedPreNode.innerText = rotateTriangle(inputNode.value);
+  reversedPreNode.innerText = rotateTriangle(
+    inputNode.value,
+    DELIMITER,
+    SYMBOL
+  );
 });
