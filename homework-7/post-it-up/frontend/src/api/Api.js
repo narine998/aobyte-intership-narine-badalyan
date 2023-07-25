8c1d3375954cea96cf3750b10d3db5a4c3feba8b
Narine <68425891+narine998@users.noreply.github.com>
2023-07-25 12:37:15 +0400

    Add .env files

--- homework-7/post-it-up/frontend/src/api/Api.js
+++ homework-7/post-it-up/frontend/src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Replace with your backend URL
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 export const fetchPosts = () => api.get("/posts");
